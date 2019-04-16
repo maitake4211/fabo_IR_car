4c2ee472dec82a76af2dd979cb7ff7227ec3f406
maitake4211 <maitakeict4211@gmail.com>
2019-04-16 19:47:49 +0900

    add Car.forard module

--- fabo_IR_car.ino
+++ fabo_IR_car.ino
@@ -1,12 +1,14 @@
 #include <IRremote.h>
 #include <Wire.h>
-#include "CarController.h"
+#include <CarController.h>
 
 int input_pin = 13; //set D10 as input signal pin
 IRrecv irrecv(input_pin);
 decode_results signals;
+
 #define ADR_DRV_L      0x64
 #define ADR_DRV_R      0x63
+CarController Car = CarController(ADR_DRV_R, ADR_DRV_L);
 
 void setup() {
   Wire.begin();
@@ -17,27 +19,16 @@ void setup() {
 
 void loop() {
   double i = signals.value;
- 
+
   if (irrecv.decode(&signals)) {
     Serial.println(i);
-    if(i > 17000000.00 ){     //ovfを検知
+    if (i > 17000000.00 ) {   //ovfを検知
       Serial.println("ovf");
+    } else if (i == 16718055.00) {
+      Car.Forward();
+      delay(2000);
     }
     delay(100);
-    else if(i == 16750695.00){
-      Car.Forward();
-    }
-    else if(i == ){
-      Car.Reverse();
-    }
-    else if(i == ){
-       Car.TurnRight();
-    }
-    else if(i == ){
-      Car.TurnLeft();
-    }
-    else if(i == ){
-      Car.Stop();
-    }
+    irrecv.resume();
   }
 }
